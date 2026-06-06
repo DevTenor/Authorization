34862d80dae7d53917449f1f33ec1d0b8e04aeeb
Ivan Stavytskyi <ivan.stavytskyi@student.tuke.sk>
2026-06-06 17:52:04 +0200

    Authorization with JWT saved in localstorage

--- backend/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) UNIQUE,
-    encoded_password VARCHAR(128),
-    first_name VARCHAR(128),
-    last_name VARCHAR(128),
-    mobile_phone VARCHAR(20),
-    created_at timestamptz default current_timestamp
-);
